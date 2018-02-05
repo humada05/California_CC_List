e2efc22c06712f5a9c3f5fb2532bd3ae2bc1c94f
Ahmed Husain <ah27182@berkeley.edu>
2018-02-05 04:09:22 -0800

    displays list, some links still poke out of table and about page not good

---
+++ backend.py
@@ -0,0 +1,29 @@
+import requests, validators
+from bs4 import BeautifulSoup
+
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRA-Tgc9b4jXCEv49D0qX9gmbJpP5xv9TwLCGtgJWGxdA4sdvGclk_AwQeH0fD4VhGnyS8caeV6Os8G/pubhtml?gid=0&single=true'
+
+def retrieve_data():
+    try:
+
+        full_html = requests.get(url)
+        parsed_html = BeautifulSoup(full_html.content, 'lxml')
+
+        table_html = parsed_html.find('table')
+
+        table_html['class'] = 'table table-hover'
+        table_html['style'] = "table-layout: fixed; width: 100%;"
+        del table_html.contents[0]
+        table = table_html.contents[0]
+
+        del table.contents[1]
+
+        for row in table.contents:
+            del row.contents[0]
+        
+
+        return table_html
+
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        return "Page not found"
